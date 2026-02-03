9974284b4e8baecb78358711ca38fed55de338d3
Your Name <qa.engineer.hamster@gmail.com>
2026-02-03 15:40:28 +0300

    Update tests

--- tests.py
+++ tests.py
@@ -40,61 +40,68 @@ class TestBooksCollector:
         assert len(collector.get_books_genre()) == 0
 
     # 4. Установка жанра существующей книге
-    def test_set_book_genre_success(self):
+    def test_set_book_genre_sets_genre(self):
         collector = BooksCollector()
 
         collector.add_new_book('1984')
         collector.set_book_genre('1984', 'Фантастика')
 
+        assert collector.books_genre['1984'] == 'Фантастика'
+
+    # 5. Получение жанра книги
+    def test_get_book_genre_returns_genre(self):
+        collector = BooksCollector()
+
+        collector.books_genre['1984'] = 'Фантастика'
+
         assert collector.get_book_genre('1984') == 'Фантастика'
 
-    # 5. Нельзя установить жанр, которого нет в списке
+    # 6. Нельзя установить жанр, которого нет в списке
     def test_set_invalid_genre(self):
         collector = BooksCollector()
 
         collector.add_new_book('1984')
         collector.set_book_genre('1984', 'Роман')
 
-        assert collector.get_book_genre('1984') == ''
+        assert collector.books_genre['1984'] == ''
 
-    # 6. Получение книг по конкретному жанру
+    # 7. Получение книг по конкретному жанру
     def test_get_books_with_specific_genre(self):
         collector = BooksCollector()
 
-        collector.add_new_book('Оно')
-        collector.add_new_book('Сияние')
-        collector.set_book_genre('Оно', 'Ужасы')
-        collector.set_book_genre('Сияние', 'Ужасы')
+        collector.books_genre = {
+            'Оно': 'Ужасы',
+            'Сияние': 'Ужасы'
+        }
 
         books = collector.get_books_with_specific_genre('Ужасы')
 
         assert books == ['Оно', 'Сияние']
 
-    # 7. Книги с возрастным рейтингом не попадают в детские
+    # 8. Книги с возрастным рейтингом не попадают в детские
     def test_books_for_children_without_age_rating(self):
         collector = BooksCollector()
 
-        collector.add_new_book('Чип и Дейл')
-        collector.add_new_book('Оно')
-
-        collector.set_book_genre('Чип и Дейл', 'Мультфильмы')
-        collector.set_book_genre('Оно', 'Ужасы')
+        collector.books_genre = {
+            'Чип и Дейл': 'Мультфильмы',
+            'Оно': 'Ужасы'
+        }
 
         children_books = collector.get_books_for_children()
 
         assert 'Чип и Дейл' in children_books
         assert 'Оно' not in children_books
 
-    # 8. Добавление книги в избранное
+    # 9. Добавление книги в избранное
     def test_add_book_to_favorites(self):
         collector = BooksCollector()
 
         collector.add_new_book('Шерлок Холмс')
         collector.add_book_in_favorites('Шерлок Холмс')
 
-        assert 'Шерлок Холмс' in collector.get_list_of_favorites_books()
+        assert 'Шерлок Холмс' in collector.favorites
 
-    # 9. Нельзя добавить книгу в избранное дважды
+    # 10. Нельзя добавить книгу в избранное дважды
     def test_add_same_book_to_favorites_twice(self):
         collector = BooksCollector()
 
@@ -102,14 +109,23 @@ class TestBooksCollector:
         collector.add_book_in_favorites('Шерлок Холмс')
         collector.add_book_in_favorites('Шерлок Холмс')
 
-        assert len(collector.get_list_of_favorites_books()) == 1
+        assert len(collector.favorites) == 1
 
-    # 10. Удаление книги из избранного
+    # 11. Удаление книги из избранного
     def test_delete_book_from_favorites(self):
         collector = BooksCollector()
 
-        collector.add_new_book('Шерлок Холмс')
-        collector.add_book_in_favorites('Шерлок Холмс')
+        collector.favorites = ['Шерлок Холмс']
         collector.delete_book_from_favorites('Шерлок Холмс')
 
-        assert collector.get_list_of_favorites_books() == []
+        assert collector.favorites == []
+
+    # 12. Получение списка избранных книг
+    def test_get_list_of_favorites_books_returns_list(self):
+        collector = BooksCollector()
+
+        collector.favorites = ['Шерлок Холмс']
+
+        favorites = collector.get_list_of_favorites_books()
+
+        assert favorites == ['Шерлок Холмс']
